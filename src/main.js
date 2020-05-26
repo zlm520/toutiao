f2e89cc8cfed3ac54628fff02bdcf5a8fedc8ad3
周玲敏 <946880300@qq.com>
2020-05-26 10:37:51 +0800

    调整初始目录结

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import style from './style/index'
 
 Vue.config.productionTip = false
 
 new Vue({
+  style,
   router,
   store,
   render: h => h(App)
