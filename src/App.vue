4e630ea6f25ec8d901970526282bc553a253b365
周玲敏 <946880300@qq.com>
2020-05-26 15:06:50 +0800

    vant配置

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="app">
-   
+ 11
+ <i class="toutiao toutiao-dianzan2 "></i>
+ <van-button type="default">默认按钮</van-button>
+<van-button type="primary">主要按钮</van-button>
+<van-button type="info">信息按钮</van-button>
+<van-button type="warning">警告按钮</van-button>
+<van-button type="danger">危险按钮</van-button>
+
     <router-view/>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import style from './style/index'
+import style from './style/index.less'
+// 引入vant 组件库
+import Vant from 'vant';
+// 引入vant css样式
+import 'vant/lib/index.css';
+
+
+Vue.use(Vant);
 
 Vue.config.productionTip = false
 
